ef4bb7aff30318458c8fd4644453fb91854bfc6b
renan jegouzo <renan@aestesis.org>
2026-01-25 03:10:24 +0100

    first commit

---
+++ example/route_directions_example.dart
@@ -0,0 +1,6 @@
+import 'package:route_directions/route_directions.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/route_directions_test.dart
@@ -0,0 +1,16 @@
+import 'package:route_directions/route_directions.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
